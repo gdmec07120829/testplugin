08259612a86f5c5e522978a104b816eccbbbb68a
gdmec07120829 <907941951@qq.com>
2014-06-16 11:34:05 +0800

    07120829

---
+++ platforms/android/assets/www/plugins/com.rensanning.cordova.helloworld/www/hello_world.js
@@ -0,0 +1,9 @@
+cordova.define("com.rensanning.cordova.helloworld.helloworld", function(require, exports, module) {var HelloWorld = function() {};  
+  
+HelloWorld.prototype.say = function() {  
+    alert("Hello World");  
+};  
+  
+var helloWorld = new HelloWorld();  
+module.exports = helloWorld;  
+});
